f43aa97e703acac2b00189ae648c611d11af269c
raphzam <raphaeljasonzamora@gmail.com>
2020-06-14 22:17:37 -0400

    added Letter

---
+++ src/Letter.java
@@ -0,0 +1,21 @@
+public class Letter {
+    public static void main(String[] args) {
+        //declaring variables
+
+        String country = "Philippines";     //name of country
+        double population = 1.67;           //population in millions
+        String capCity = "Manila";          //capital city
+        double phP = 50.26;                 //US dollar to Philippine peso
+
+        System.out.println("Hey Mom and Dad!");
+        System.out.println("Here are some interesting details about the first country I'm visiting.");
+        System.out.println("Name: " + country);
+        System.out.println("Population Size: " + population + " million.");
+        System.out.println("Capital: " + capCity);
+        System.out.println("Currency: One U.S Dollar equals " + "$" + phP + " Philippine pesos!!");
+        System.out.println("I'm so excited to visit the " + capCity + " National Park and feel the zebras!! Talk later.");
+
+
+
+    }
+}
