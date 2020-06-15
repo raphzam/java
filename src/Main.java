ca203162eb25036af50b2cbeabb5b7bd2895e6e8
raphzam <raphaeljasonzamora@gmail.com>
2020-06-14 21:52:37 -0400

    Added Main and Health

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String patientName = "Stacey Manson";
+        boolean hasInsurance = false;
+        double temp = 102;
+        double numVisits = 5.02;
+        double copay = 25;
+        String nextApp = "6/25/20";
+        String phoneNum = "301 - 555 - 3800";
+
+        System.out.println("Patient Details: ");
+        System.out.println("Name: " + patientName);
+        System.out.println("Patient has insurance: " + hasInsurance);
+        System.out.println("Body temperature: " + temp);
+        System.out.println("Number of visits: " + numVisits);
+        System.out.println("Patient's copay amount: " + "$" + copay );
+        System.out.println("Next appointment date: " + nextApp );
+        System.out.println("Contact number: " + phoneNum );
+
+
+    }
+}
