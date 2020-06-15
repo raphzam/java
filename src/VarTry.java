cceaf8ec19ec3f8d91e308cb71b8499dab5804fc
raphzam <raphaeljasonzamora@gmail.com>
2020-06-14 21:25:57 -0400

    initial commit

---
+++ src/VarTry.java
@@ -0,0 +1,10 @@
+public class VarTry {
+    public static void main(String[] args) {
+        int score = 50;
+        double price = 24.99;
+        String name = "Bobby Johnson";
+
+        System.out.println("Test Score intialized is: " + score);
+        System.out.println("Price tag: " + price);
+    }
+}
