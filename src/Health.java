3973c88f26934e8799868e7a4de8aaf8403fe247
raphzam <raphaeljasonzamora@gmail.com>
2020-06-14 21:57:16 -0400

    finished Health

--- src/Health.java
+++ src/Health.java
@@ -1,2 +1,16 @@
 public class Health {
+
+    public static void main(String[] args) {
+        int workOutDays = 10;
+        double health = 8.5;
+        boolean powerUp = true;
+
+        System.out.println("Number of Days: " + workOutDays);
+        System.out.println("Health Scale: " + health);
+        System.out.println("Are you ready to power up? " + powerUp);
+        System.out.println("Total Assessment: It has been " + workOutDays + " since you've started working out.");
+        System.out.println("You evaulate at " + health + " out of 10 on the health scale.");
+        System.out.println("It is " + powerUp + " that you are ready to power up your workout!");
+
+    }
 }
